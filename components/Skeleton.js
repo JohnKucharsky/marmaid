f045319b6098412623b377ddc19e9e8fe45929f7
Johnkuchar <vannok@live.com>
2022-02-07 01:16:44 +0300

    6

---
+++ components/Skeleton.js
@@ -0,0 +1,37 @@
+function Skeleton() {
+	return (
+		<div>
+			<div className='skeleton'>
+				<div className='s-banner'></div>
+				<div className='s-header'></div>
+				<div className='s-content'></div>
+				<div className='s-content'></div>
+				<div className='s-content'></div>
+				<style jsx>{`
+					.skeleton {
+						max-width: 1200px;
+						margin: 20px auto;
+					}
+					.skeleton > div {
+						background: #dbcc1a;
+						border-radius: 4px;
+						margin: 20px 0;
+					}
+					.s-banner {
+						padding: 12px 0;
+					}
+					.s-header {
+						padding: 15px 0;
+						max-width: 500px;
+					}
+					.s-content {
+						padding: 8px 0;
+						max-width: 1000px;
+					}
+				`}</style>
+			</div>
+		</div>
+	)
+}
+
+export default Skeleton
